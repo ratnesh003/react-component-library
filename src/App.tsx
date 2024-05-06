1f10c6523ca52b04bcd6cbf75db892932c959f37
Ratnesh Pasi <ratneshpasi03@gmail.com>
2024-05-06 18:04:15 +0530

    1st commit initialized project added tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>App</h1>
+      <Button className='font-mono' value='submit' />
+    </>
+  )
+}
+
+export default App
